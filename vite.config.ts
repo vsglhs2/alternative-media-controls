1356bbaf7c099a41538d5a9338e1f20ad839371c
vsglhs2 <vsglhs2@proton.me>
2025-05-26 09:21:07 +1000

    set sourcemap to be standalone file

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
                     entryFileNames: '[name].min.js', 
                 },
                 {
-                    sourcemap: 'inline',
+                    sourcemap: true,
                     entryFileNames: '[name].js', 
                 },
             ]
